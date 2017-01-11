9e2217a831291cfa9c476424fffc0ce9131c35f5
Dan Crews <crewsd@gmail.com>
2017-01-11 12:13:14 -0700

    1.1.2: Node4 doesn't have rest-params or spread operators

--- packages/eslint-config-vstg/rules/node4.js
+++ packages/eslint-config-vstg/rules/node4.js
@@ -2,6 +2,9 @@
 module.exports = {
   plugins: [ 'node' ],
   rules: {
+    'prefer-rest-params': 'off',
+    'prefer-spread': 'off',
+
     'node/no-unsupported-features': [ 'error', { 'version': 4 } ],
   },
 };
